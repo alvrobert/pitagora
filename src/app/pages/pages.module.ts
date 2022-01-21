40b228637ac439105730db8c3a0067d4ab1c5c92
Robert Alvarado <>
2022-01-20 22:39:17 -0300

    Agrego Modulo de persona

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,11 +4,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [    
     DashboardComponent,
     PagesComponent,
+    PersonComponent,
   ],
   exports: [
     DashboardComponent,
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PersonComponent } from './person/person.component';
 const routes: Routes = [
     { 
         path: 'dashboard', 
         component: PagesComponent,
         children: [
             { path: '', component: DashboardComponent },
+            { path: 'person', component: PersonComponent }
         ]
     },
 ];
